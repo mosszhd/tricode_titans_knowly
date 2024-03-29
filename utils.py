a9060d3482f33b44ffbfe0a54da9470b734d8dfb
mosszhd <mustafakamalmoss@gmail.com>
2024-03-30 03:41:43 +0600

    added function to get summary

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import json
 from datetime import datetime
+import ollama
 
 def save_chat_history(chat_history,session_key):
     file_name = "sessions/" + session_key
@@ -13,3 +14,37 @@ def load_chat_history_json(session_name):
     
 def get_timestamp():
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+def get_summary(session_messages):
+
+    conversation_string = "\n".join(
+        f"{message['role']}: {message['content']}" for message in session_messages
+    )
+    word_limit = max(10, len(conversation_string) // 4)
+
+    fpc = ['Knowly', 'I', 'Me', 'My', 'Mine', 'Myself']
+    spc = ['You', 'Your', 'Yours', 'Yourself', 'Yourselves']
+
+    prompt = f"""**In the following conversation,**
+
+    Rewrite the summary but this time replace any references to Assistant or system with first person perspective (Helping first person pronouns: {fpc}). And replace any references to the user in second person perspective.
+
+    {conversation_string}
+"""
+    
+    prompt_1 = f"""
+    Summarize this conversation in a single paragraph, ensuring the summary is within {word_limit} words.
+
+    Start with "Sure here is the conversation summary."
+    {prompt}
+"""
+
+    response = ollama.chat(model='gemma:2b', messages=[
+        {'role': 'system', 'content': prompt_1},
+        {'role': 'user', 'content': conversation_string},
+    ])
+
+    summary = response["message"]["content"]
+    
+    return summary
